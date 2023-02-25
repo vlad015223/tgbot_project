b56f6ce21d079d49648a54f84be62a550022ac43
Влад Туров <vlad01523@yandex.ru>
2023-02-25 16:31:15 +0300

    minor fix at buttons.py

--- buttons.py
+++ buttons.py
@@ -1,19 +1,18 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram import types
 
-main_kb = [
-        [types.KeyboardButton(text='Обрезать ютуб-видео')],
-        [types.KeyboardButton(text='Получить аудио из Я. Музыки')],
-        [types.KeyboardButton(text='Третья кнопка')],
-        [types.KeyboardButton(text='Четвертая кнопка')],
-        [types.KeyboardButton(text='Пятая кнопка')],
-        [types.KeyboardButton(text='Шестая кнопка')]
-    ]
+b1 = KeyboardButton('Перевод текста')
+b2 = KeyboardButton('Обрезать ютуб-видео')
+b3 = KeyboardButton('Отправить mp3 файл из ютуб-видео')
+b4 = KeyboardButton('Получить аудио из Я. Музыки')
+b5 = KeyboardButton('Игра камень-ножницы-бумага')
+b6 = KeyboardButton('Отправить анекдот')
+b7 = KeyboardButton('Отправить погоду')
+b8 = KeyboardButton('Режим агрессии')
 
-keyboard = types.ReplyKeyboardMarkup(
-        keyboard=main_kb,
-        resize_keyboard=True,
-        one_time_keyboard=True,
-    )
+keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+
+keyboard.row(b1, b2).row(b3, b4, b5).row(b6, b7, b8)
 
 back_to_the_main_menu = [
     [types.KeyboardButton(text='Вернуться в главное меню')]
